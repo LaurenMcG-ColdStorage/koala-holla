4fac86fe9d396c4f171ad65aabf4bc177320a370
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2023-12-23 10:41:41 -0600

    Added route skeleton, populated database.

---
+++ server/data/koala-data.js
@@ -0,0 +1,5 @@
+const koalas = [
+
+];
+
+module.exports = koalas;
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,13 +2,17 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const koalas = require('../data/koala-data');
 
 // GET
+koalaRouter.get('/',(req, res) => {
 
+});
 
 // POST
+koalaRouter.post('/', (req, res => {
 
+}));
 
 // PUT
 
